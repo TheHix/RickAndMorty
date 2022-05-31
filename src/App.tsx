be96e93e26ab4ae0c9eda5e62f054f2b03e6b0a3
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-05-31 17:51:29 +0300

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import Header from './components/header/header';
-import Main from './pages/main';
-import './styles/App.scss';
-
+import React from "react";
+import { Navigate, Route, Routes } from "react-router";
+import Header from "./components/header/header";
+import Info from "./pages/info";
+import "./styles/App.scss";
+import Home from "./pages/home";
 function App() {
   return (
     <div className="wrapper">
-      <Header/>
-      <Main/>
+      <Header />
+      <Routes>
+        <Route path = "/" element={<Home />}/>
+        <Route path = "/info" element={<Info/>}/>
+        <Route path = "*" element={<Navigate to={'/'} replace/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/home.tsx
@@ -0,0 +1,53 @@
+import { useStore } from "effector-react";
+import React, { useEffect } from "react";
+import { URL } from "../API/constants";
+import Season from "../components/Season";
+import {
+  $seasones,
+  addSeason,
+  getSeasonFx,
+  clearSeasones,
+} from "../store/store";
+import { ISeason } from "../Types/episodes";
+
+const Home: React.FC = () => {
+  const seasones = useStore($seasones);
+
+  const getAllSeasons = async (numSeasons: any) => {
+    for (let i = 1; i <= numSeasons; i++) {
+      addSeason(await getSeasonFx(i));
+    }
+  };
+
+  const getNumberOfSeasons = async () => {
+    const result = await fetch(URL.episodes);
+    const json = await result.json();
+    const numSeasons = await json.info.pages;
+    getAllSeasons(numSeasons);
+  };
+
+  useEffect(() => {
+    if (seasones.length === 0) {
+      getNumberOfSeasons();
+    }
+  }, []);
+  return (
+    <main className="main">
+      <div className="main__info info">
+        <div className="info__container container">
+          {seasones.map((currentSeasone: ISeason, index: number) => {
+            return (
+              <Season
+                key={index}
+                seasonNumber={(index + 1).toString()}
+                seasoneInfo={currentSeasone.results}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
