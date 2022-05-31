76af3b837d34f2886bb5199c92f7dbe07e688663
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-05-31 15:34:04 +0300

    added events to interact with the list of seasons

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,39 +6,31 @@ import {
   restore,
 } from "effector";
 import { URL } from "../API/constants";
-interface IEpisode {
-  id: number;
-  air_date: string;
-  name: string;
-  episode: string;
-  characters: string[];
-}
-interface ISeason {
-  info?: {
-    pages?: number;
-  };
-  results: IEpisode[];
-}
-interface ISeasonInfo {
-  seasonInfo: ISeason;
-}
-export const $episodes = createStore<IEpisode[]>([]);
+import { IEpisode, ISeason } from "../Types/episodes";
+
+export const addSeason = createEvent<ISeason>();
+export const seasonsFilter = createEvent<number>();
 export const season = createEvent<IEpisode>();
-export const getSeason = createEffect(async (page: number = 1) => {
+export const setSeasones = createEvent();
+export const getSeasonFx = createEffect(async (page: number = 1) => {
   try {
-    const response = await fetch(
-      `${URL.episodes}?page=${page}`
-    );
+    const response = await fetch(`${URL.episodes}?page=${page}`);
     return response.json();
   } catch (error) {
     console.log(error);
   }
-  
 });
 
-export default createStore<ISeasonInfo>({
-  seasonInfo: {
-    info: {},
-    results: [],
-  },
-}).on(getSeason.doneData, (state, season) => ({ ...state, season }));
+export const $currentSeason = createStore<ISeason>({
+  info: {},
+  results: [],
+}).on(getSeasonFx.doneData, (_, season) => season);
+
+export const $seasones = createStore<ISeason[]>([])
+  .on(setSeasones, () => {})
+  .on(addSeason, (state, newSeson) => [...state, newSeson])
+  .on(seasonsFilter, (state, numSeasone: number) => {
+    return state.filter((seasone: ISeason) => {
+      return seasone.info?.pages !== numSeasone;
+    });
+  });
