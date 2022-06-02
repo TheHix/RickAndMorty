73f908f66353d333913aad6fecaa003a1778af75
Ssaveliy <saveliy-sokolnikov@mail.ru>
2022-06-02 05:35:45 +0300

    loader component created

---
+++ src/components/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Loader:React.FC = () => {
+    return (
+        <div style={{display: 'flex', justifyContent: 'center', margin: '5rem'}}>
+            <div className="lds-ring">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Loader;
